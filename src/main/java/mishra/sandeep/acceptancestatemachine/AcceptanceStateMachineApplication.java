ab0e98fc95fd1ea3bdb4c92bfc3cf449d8ec481a
Sandeep Mishra <sandeep.r.mishra@live.com>
2020-06-10 09:10:23 +0800

    fixing entity scan

--- src/main/java/mishra/sandeep/acceptancestatemachine/AcceptanceStateMachineApplication.java
+++ src/main/java/mishra/sandeep/acceptancestatemachine/AcceptanceStateMachineApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("mishra.sandeep.bookingstatemachine.booking")
+@EntityScan("mishra.sandeep.acceptancestatemachine.model")
 public class AcceptanceStateMachineApplication {
 
 	public static void main(String[] args) {
